1e70015dbea416788c4afb55164564d473a41929
Snap888 <dolenor1@gmail.com>
2025-11-27 23:21:09 +0300

    Update ControlsProfile.java

--- app/src/main/java/com/winlator/cmod/inputcontrols/ControlsProfile.java
+++ app/src/main/java/com/winlator/cmod/inputcontrols/ControlsProfile.java
@@ -205,8 +205,7 @@ public class ControlsProfile implements Comparable<ControlsProfile> {
             JSONArray elementsJSONArray = profileJSONObject.getJSONArray("elements");
             for (int i = 0; i < elementsJSONArray.length(); i++) {
                 JSONObject elementJSONObject = elementsJSONArray.getJSONObject(i);
-                ControlElement element = new ControlElement(inputControlsView);
-                element.setType(ControlElement.Type.valueOf(elementJSONObject.getString("type")));
+                ControlElement element = new ControlElement(inputControlsView, elementJSONObject);
                 element.setShape(ControlElement.Shape.valueOf(elementJSONObject.getString("shape")));
                 element.setToggleSwitch(elementJSONObject.getBoolean("toggleSwitch"));
                 element.setX((int)(elementJSONObject.getDouble("x") * inputControlsView.getMaxWidth()));
@@ -214,6 +213,88 @@ public class ControlsProfile implements Comparable<ControlsProfile> {
                 element.setScale((float)elementJSONObject.getDouble("scale"));
                 element.setText(elementJSONObject.getString("text"));
                 element.setIconId(elementJSONObject.getInt("iconId"));
+                
+                // Load profile switching settings
+                if (elementJSONObject.has("enableProfileSwitching")) {
+                    element.setEnableProfileSwitching(elementJSONObject.getBoolean("enableProfileSwitching"));
+                }
+                if (elementJSONObject.has("targetProfileId")) {
+                    element.setTargetProfileId(elementJSONObject.getInt("targetProfileId"));
+                }
+                if (elementJSONObject.has("switchDelay")) {
+                    element.setSwitchDelay((float)elementJSONObject.getDouble("switchDelay"));
+                }
+                
+                // Load DYNAMIC_STICK specific settings
+                if (element.getType() == ControlElement.Type.DYNAMIC_STICK) {
+                    if (elementJSONObject.has("stickActivationRadius")) {
+                        element.setStickActivationRadius((float)elementJSONObject.getDouble("stickActivationRadius"));
+                    }
+                    
+                    // Load new activation zone settings
+                    if (elementJSONObject.has("showActivationZone")) {
+                        element.setShowActivationZone(elementJSONObject.getBoolean("showActivationZone"));
+                    }
+                    if (elementJSONObject.has("activationZoneWidth")) {
+                        element.setActivationZoneWidth((float)elementJSONObject.getDouble("activationZoneWidth"));
+                    }
+                    if (elementJSONObject.has("activationZoneHeight")) {
+                        element.setActivationZoneHeight((float)elementJSONObject.getDouble("activationZoneHeight"));
+                    }
+                }
+                
+                // Load Vertical Scroll Bar specific settings
+                if (element.getType() == ControlElement.Type.VERTICAL_SCROLL_BAR) {
+                    if (elementJSONObject.has("scrollBarWidth")) {
+                        element.setScrollBarWidth((float)elementJSONObject.getDouble("scrollBarWidth"));
+                    }
+                    if (elementJSONObject.has("scrollBarHeight")) {
+                        element.setScrollBarHeight((float)elementJSONObject.getDouble("scrollBarHeight"));
+                    }
+                    if (elementJSONObject.has("scrollSensitivity")) {
+                        element.setScrollSensitivity((float)elementJSONObject.getDouble("scrollSensitivity"));
+                    }
+                }
+                
+                // Load custom icon data if present
+                if (elementJSONObject.has("customIconId")) {
+                    element.setCustomIconId(elementJSONObject.getString("customIconId"));
+                }
+                if (elementJSONObject.has("hasCustomIcon")) {
+                    element.setHasCustomIcon(elementJSONObject.getBoolean("hasCustomIcon"));
+                }
+                
+                // Load icon size multiplier if present
+                if (elementJSONObject.has("iconSizeMultiplier")) {
+                    element.setIconSizeMultiplier((float)elementJSONObject.getDouble("iconSizeMultiplier"));
+                }
+                
+                // Load opacity settings if present
+                if (elementJSONObject.has("buttonOpacity")) {
+                    element.setButtonOpacity((float)elementJSONObject.getDouble("buttonOpacity"));
+                }
+                if (elementJSONObject.has("iconOpacity")) {
+                    element.setIconOpacity((float)elementJSONObject.getDouble("iconOpacity"));
+                }
+                
+                // Load outline visibility if present
+                if (elementJSONObject.has("showOutline")) {
+                    element.setShowOutline(elementJSONObject.getBoolean("showOutline"));
+                }
+                
+                // Load MultiBinding settings if present
+                if (elementJSONObject.has("useMultiBinding")) {
+                    element.setUseMultiBinding(elementJSONObject.getBoolean("useMultiBinding"));
+                    if (element.isUseMultiBinding() && elementJSONObject.has("multiBindings")) {
+                        JSONArray multiBindingsArray = elementJSONObject.getJSONArray("multiBindings");
+                        for (int j = 0; j < multiBindingsArray.length() && j < 4; j++) {
+                            JSONObject multiBindingJSON = multiBindingsArray.getJSONObject(j);
+                            MultiBinding multiBinding = MultiBinding.fromJSONObject(multiBindingJSON);
+                            element.setMultiBindingAt(j, multiBinding);
+                        }
+                    }
+                }
+                
                 if (elementJSONObject.has("range")) element.setRange(ControlElement.Range.valueOf(elementJSONObject.getString("range")));
                 if (elementJSONObject.has("orientation")) element.setOrientation((byte)elementJSONObject.getInt("orientation"));
 
@@ -233,5 +314,116 @@ public class ControlsProfile implements Comparable<ControlsProfile> {
         catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Check if this profile has any elements with profile switching enabled
+     * @return true if at least one element has profile switching enabled
+     */
+    public boolean hasProfileSwitchingElements() {
+        if (!elementsLoaded) return false;
+        
+        for (ControlElement element : elements) {
+            if (element.isEnableProfileSwitching() && element.getTargetProfileId() != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get list of elements that have profile switching enabled
+     * @return list of profile switching elements
+     */
+    public List<ControlElement> getProfileSwitchingElements() {
+        List<ControlElement> switchingElements = new ArrayList<>();
+        if (!elementsLoaded) return switchingElements;
+        
+        for (ControlElement element : elements) {
+            if (element.isEnableProfileSwitching() && element.getTargetProfileId() != 0) {
+                switchingElements.add(element);
+            }
+        }
+        return switchingElements;
+    }
+
+    /**
+     * Get the target profile ID for a specific element
+     * @param elementId the element ID to check
+     * @return target profile ID, or 0 if not found or not enabled
+     */
+    public int getTargetProfileIdForElement(String elementId) {
+        if (!elementsLoaded) return 0;
+        
+        for (ControlElement element : elements) {
+            if (element.getId().equals(elementId) && 
+                element.isEnableProfileSwitching() && 
+                element.getTargetProfileId() != 0) {
+                return element.getTargetProfileId();
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Check if this profile has any elements with multi-binding enabled
+     * @return true if at least one element has multi-binding enabled
+     */
+    public boolean hasMultiBindingElements() {
+        if (!elementsLoaded) return false;
+        
+        for (ControlElement element : elements) {
+            if (element.isUseMultiBinding() && !element.getMultiBindingAt(0).isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get list of elements that have multi-binding enabled
+     * @return list of multi-binding elements
+     */
+    public List<ControlElement> getMultiBindingElements() {
+        List<ControlElement> multiBindingElements = new ArrayList<>();
+        if (!elementsLoaded) return multiBindingElements;
+        
+        for (ControlElement element : elements) {
+            if (element.isUseMultiBinding() && !element.getMultiBindingAt(0).isEmpty()) {
+                multiBindingElements.add(element);
+            }
+        }
+        return multiBindingElements;
+    }
+
+    /**
+     * Check if this profile has any Vertical Scroll Bar elements
+     * @return true if at least one element is a Vertical Scroll Bar
+     */
+    public boolean hasVerticalScrollBarElements() {
+        if (!elementsLoaded) return false;
+        
+        for (ControlElement element : elements) {
+            if (element.getType() == ControlElement.Type.VERTICAL_SCROLL_BAR) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get list of Vertical Scroll Bar elements
+     * @return list of Vertical Scroll Bar elements
+     */
+    public List<ControlElement> getVerticalScrollBarElements() {
+        List<ControlElement> scrollBarElements = new ArrayList<>();
+        if (!elementsLoaded) return scrollBarElements;
+        
+        for (ControlElement element : elements) {
+            if (element.getType() == ControlElement.Type.VERTICAL_SCROLL_BAR) {
+                scrollBarElements.add(element);
+            }
+        }
+        return scrollBarElements;
     }
 }
